82e3ee2cb1f94bc093e34f6e38d2937b9ebf32d6
Dawid Oberda <=>
2020-07-30 15:08:40 +0200

    qrcode_scanner.py working on

--- image_analyzer/live_image.py
+++ image_analyzer/live_image.py
@@ -3,33 +3,35 @@ import imutils
 import cv2
 import time
 
-# vs = VideoStream(src=0).start()
-# time.sleep(1.0)
-
-while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=800, height=600)
-    cv2.imshow('Live image', frame)
-
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-
-cv2.destroyAllWindows()
-vs.stop()
-
-
 class ImageStream:
 
     vs = None
 
-    def __init__(self,source):
+    def __init__(self, source):
         self.source = source
 
     def start_stream(self):
-        self.vs = VideoStream(self.source).start()
+        self.vs = VideoStream(src = self.source).start()
         time.sleep(1.0)
 
-        # TODO: zrobic metode ktora pokazuje obraz. sprawdzic tez jak zrobic aby zatrzymywala sie na jakis inny sygnal oprocz przycisku
+        while True:
+            frame = self.vs.read()
+            frame = imutils.resize(frame, width=800, height=600)
+            cv2.imshow('Live image', frame)
+
+            key = cv2.waitKey(1)
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
+            elif key == 27:
+                break
+
+        cv2.destroyAllWindows()
+        self.vs.stop()
+
+
+if __name__ == "__main__":
+    im = ImageStream(0)
+
+    im.start_stream()
--- qrcode_tools/qrcode_scanner.py
+++ qrcode_tools/qrcode_scanner.py
@@ -1,1 +1,48 @@
 #https://www.pyimagesearch.com/2018/05/21/an-opencv-barcode-and-qr-code-scanner-with-zbar/
+
+from imutils.video import VideoStream
+import imutils
+import cv2
+import time
+from pyzbar.pyzbar import decode
+
+vs = None
+
+vs = VideoStream(src = 0).start()
+time.sleep(1.0)
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=800, height=600)
+
+
+    barcodes = decode(frame)
+
+    for barcode in barcodes:
+        x, y, width, height = barcode.rect
+        print(x)
+        print(y)
+        print(width)
+        print(height)
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 2)
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+
+        print(barcodeData)
+
+        text = "{} ({})".format(barcodeData, barcodeType)
+        cv2.putText(frame, text, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    cv2.imshow('Live image', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+    elif key == 27:
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
+
