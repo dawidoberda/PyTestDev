6b5444d248e11794c09d2c6ec7c2d699d3294591
Dawid Oberda <=>
2020-10-16 15:38:15 +0200

    working on json requests

--- json_request.py
+++ json_request.py
@@ -5,23 +5,35 @@ import os
 
 
 
-def post_request_from_config(self, file):
+def post_request_from_config(tests_dir, test_seq):
+
+    step_list = []
 
     try:
-        with open(file) as f:
-            request = f.read()
+        with open(test_seq) as seq:
+            tests = json.load(f)
     except FileNotFoundError as fnfe:
         print(str(fnfe))
         exit(1)
 
-    print(request)
-
-    # response = requests.post('http://192.168.0.90/papi/index.html', json={'id': 1, 'name': 'Jessa'})
+    # try:
+    #     with open(file) as f:
+    #         request = json.load(f)
+    # except FileNotFoundError as fnfe:
+    #     print(str(fnfe))
+    #     exit(1)
+    #
+    # print(request)
+    #
+    #
+    # response = requests.post('http://192.168.0.90/axis-cgi/papi/commands', json = request)
     #
     # print("Status code: ", response.status_code)
     # print("Printing Entire Post Request")
     # print(response.json())
 
+
 if __name__ == "__main__":
-    file = os.path.join("C:\Users\oberdad\OneDrive - Jabil\Dawid\TE\Axis\json_req", "sample.txt")
-    post_request_from_config()
+    tests_dir = "C:\\Users\\oberdad\\OneDrive - Jabil\\Dawid\\TE\\Axis\\json_req\\tests"
+    test_seq = os.path.join("C:\\Users\\oberdad\\OneDrive - Jabil\\Dawid\\TE\\Axis\\json_req", "sample_test_seq.txt")
+    post_request_from_config(tests_dir=tests_dir, test_seq=test_seq)
