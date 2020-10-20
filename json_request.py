16e3b650c087b5e9fe56696b172e19511362079a
Dawid Oberda <=>
2020-10-20 15:11:28 +0200

    working on json requests

--- json_request.py
+++ json_request.py
@@ -9,28 +9,46 @@ def post_request_from_config(tests_dir, test_seq):
 
     step_list = []
 
+    #LOADING TEST SEQ
     try:
         with open(test_seq) as seq:
-            tests = json.load(f)
+            for line in seq:
+                line = line.rstrip('\n')
+                step_list.append(str(line))
     except FileNotFoundError as fnfe:
         print(str(fnfe))
         exit(1)
 
-    # try:
-    #     with open(file) as f:
-    #         request = json.load(f)
-    # except FileNotFoundError as fnfe:
-    #     print(str(fnfe))
-    #     exit(1)
-    #
-    # print(request)
-    #
-    #
-    # response = requests.post('http://192.168.0.90/axis-cgi/papi/commands', json = request)
-    #
-    # print("Status code: ", response.status_code)
-    # print("Printing Entire Post Request")
-    # print(response.json())
+    print(step_list)
+
+    #EXECUTING SEQ
+    for step in step_list:
+        step_file = os.path.join(tests_dir, step+".txt")
+        print(step_file)
+        exist = os.path.exists(step_file)
+        print(exist)
+
+        if not exist:
+            print("file not found !!")
+            exit(1)
+
+        try:
+            with open(step_file) as f:
+                request = json.load(f)
+        except FileNotFoundError as fnfe:
+            print(str(fnfe))
+            exit(1)
+
+        print(request)
+
+
+        response = requests.post('http://192.168.0.90/axis-cgi/papi/commands', json = request)
+
+        print("Status code: ", response.status_code)
+        print("Printing Entire Post Request")
+        print(response.json())
+
+        #zrobic pliki konfiguracyjne co oznacza pass dla danego kroku i sprawdzanie tego w kodzie
 
 
 if __name__ == "__main__":
